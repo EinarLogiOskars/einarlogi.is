1f274efb9e0b216675747781edc981b7aee812d6
EinarLogiOskars <einarlogi1@gmail.com>
2025-02-10 21:17:00 +0000

    about on the homepage added

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className={styles.hero}>
         <h1 className={styles.heroTitle}>Einar Logi</h1>
+        <p className={styles.about}>This is my personal platform where I will keep anything and everything I am working on or interested in.</p>
     </main>
   );
 }
