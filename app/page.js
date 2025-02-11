700925206e3669ecbc91b95f0c486ccb2b2fe6e6
EinarLogiOskars <einarlogi1@gmail.com>
2025-02-11 02:04:14 +0000

    workshopping the homepage

--- app/page.js
+++ app/page.js
@@ -1,10 +1,23 @@
-import styles from "@/app/styles/home.module.css";
+'use client';
+
+import styles from './styles/home.module.css';
 
 export default function Home() {
   return (
     <main className={styles.hero}>
-        <h1 className={styles.heroTitle}>Einar Logi</h1>
-        <p className={styles.about}>This is my personal platform where I will keep anything and everything I am working on or interested in.</p>
+      <div className={styles.container}>
+        <header className={styles.header}>
+          <h5 className={styles.title}>Einar Logi</h5>
+        </header>
+        <section className={styles.mainSection}>
+          <p className={styles.iAm}>
+            I am
+          </p>
+          <p className={styles.name}>
+            Einar Logi.
+          </p>
+        </section>
+      </div>
     </main>
   );
 }
