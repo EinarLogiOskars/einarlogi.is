4c2c5baa76c13ad4a59a749b64f84148821d1b65
EinarLogiOskars <einarlogi1@gmail.com>
2025-02-16 14:34:16 +0000

    trying to fix a locale routing issue

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -5,19 +5,34 @@ export const routing = defineRouting({
     // A list of all locales that are supported
     locales: ['en', 'is'],
 
-    //used when no locale matches
-    defaultLocale: 'is',
-
     domains: [
         {
             domain: 'einarlogi.com',
             defaultLocale: 'en',
         },
         {
+            domain: 'www.einarlogi.com',
+            defaultLocale: 'en',
+        },
+        {
             domain: 'einarlogi.is',
             defaultLocale: 'is',
         },
+        {
+            domain: 'www.einarlogi.is',
+            defaultLocale: 'is',
+        },
+        {
+            domain: "localhost",
+            defaultLocale: "is",
+        },
+        {
+            domain: "localhost:3000",
+            defaultLocale: "is",
+        },
     ],
+
+    defaultLocale: 'is'
 });
 
 export const {Link, redirect, usePathname, useRouter, getPathname} = createNavigation(routing);
