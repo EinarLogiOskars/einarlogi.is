572cb4998191444db95aa1fe364d98a070eaae78
Einar Logi Óskarsson <einarlogi1@gmail.com>
2025-02-24 19:28:40 +0000

    Update BaseLayout.jsx
    Adding meta tag

--- components/BaseLayout.jsx
+++ components/BaseLayout.jsx
@@ -6,22 +6,25 @@ import Header from "./Header/Header";
 import MobileNav from "./MobileNav/MobileNav";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Head from "next/head"; // Import next/head for meta tags
 
-
-export default async function BaseLayout({ children, locale}) {
+export default async function BaseLayout({ children, locale }) {
     const messages = await getMessages();
 
     return (
-    <html lang={locale} className={regime.variable}>
-        <body>
-            <NextIntlClientProvider messages={messages}>
-                <MobileNav />
-                <Header />
-                <Analytics />
-                <SpeedInsights />
-                {children}
-            </NextIntlClientProvider>
-        </body>
-    </html>
-    )
+        <html lang={locale} className={regime.variable}>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+            </Head>
+            <body>
+                <NextIntlClientProvider messages={messages}>
+                    <MobileNav />
+                    <Header />
+                    <Analytics />
+                    <SpeedInsights />
+                    {children}
+                </NextIntlClientProvider>
+            </body>
+        </html>
+    );
 }
